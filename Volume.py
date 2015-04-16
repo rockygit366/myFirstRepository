335f0ba524bcb63229fbfb55fc2c82da75a35f55
rockygit366 <rocky366@gmail.com>
2015-04-16 17:26:54 +0800

    My first commit

---
+++ Volume.py
@@ -0,0 +1,16 @@
+class Volume:
+   'Volume object'
+   volCount = 0
+
+   def __init__(self, name, parent, size, fs):
+      self.name = name
+      self.parent = parent
+      self.size = size
+      self.fs = fs
+      Volume.volCount += 1
+	  
+   def displayVolume(self):
+      print "Total Volume %d" % Volume.volCount
+
+   def displayArray(self):
+      print "Name : ", self.name,  ", Parent: ", self.parent,  ", Size: ", self.size
